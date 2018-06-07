0ccc2d7ef3a1b5d3832432fe642e44eb4fd08951
Tim Golden <mail@timgolden.me.uk>
2018-06-07 20:35:39 +0100

    PUts pixel on grid and draws grid.

--- draw_art.py
+++ draw_art.py
@@ -1,4 +1,7 @@
+import board
+
 characters = [" ", ".", "-", "&", "#", "*"]
+grid = board.Board((8, 8))
 
 def map_brightness_to_char(brightness):
     n_slots = len(characters)
@@ -8,3 +11,32 @@ def map_brightness_to_char(brightness):
     else:
         slot = int(brightness/slot_width)
     return characters[slot]
+
+def put_pixel_on_grid(coord, brightness):
+    character = map_brightness_to_char(brightness)
+    grid[coord] = character
+
+def draw_grid():
+    grid.draw(use_borders=False)
+
+if __name__ == '__main__':
+    art = """\
+   ##
+  #--#
+ #----#
+  #--#
+   ##
+"""
+
+    for y, line in enumerate(art.splitlines()):
+        for x, char in enumerate(line):
+            if char == " ":
+                brightness = 0
+            elif char == "#":
+                brightness = 0.85
+            elif char == "-":
+                brightness= 0.4
+
+            put_pixel_on_grid((x,y), brightness)
+
+    draw_grid()
